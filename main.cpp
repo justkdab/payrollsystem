e1aa42dde4e7e1b43f024b17e473057d2e3e9c86
King David Benyah <35106214-kingdavbenyah@users.noreply.replit.com>
2024-07-08 12:27:06 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double hours, rate, gross, tax, net;
+    const double TAX_RATE = 0.25;
+
+    cout << "Enter the number of hours worked: ";
+    cin >> hours;
+
+    cout << "Enter the hourly pay rate: ";
+    cin >> rate;
+
+    gross = hours * rate;
+    tax = gross * TAX_RATE;
+    net = gross - tax;
+
+    cout << fixed << setprecision(2);
+    cout << "\nPayroll Calculation:" << endl;
+    cout << "Hours worked: " << hours << endl;
+    cout << "Hourly pay rate: $" << rate << endl;
+    cout << "Gross pay: $" << gross << endl;
+    cout << "Tax: $" << tax << endl;
+    cout << "Net pay: $" << net << endl;
+
+    return 0;
+}
